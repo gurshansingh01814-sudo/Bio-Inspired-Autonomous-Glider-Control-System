3ff445ff914e512d01c567d67baf3c4e5d88ebf8
gurshansingh01814-sudo <gurshansingh01814@gmail.com>
2025-10-18 22:08:57 +0530

    update w climb and distt

--- src/mpc_controller.py
+++ src/mpc_controller.py
@@ -130,9 +130,9 @@ class MPCController:
         J = 0 
         
         # Define Tuning Weights
-        W_CLIMB = 6.0         
+        W_CLIMB = 50.0         
         W_SMOOTH = 1      
-        W_DIST = 1.0          
+        W_DIST = 0.25        
         W_SLACK = 5000.0 
         W_AIRSPEED = 1     
 
